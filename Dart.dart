34fe94077a2ae1fce7850de270924c6d2e887135
Vitinn04 <victor.henrique08102004@gmail.com>
2023-06-12 00:21:31 -0300

    colocando mais lições feitas

--- Dart.dart
+++ Dart.dart
@@ -1,3 +1,5 @@
+import 'dart:mirrors';
+
 void main() {
   // 1 - Introdução a variaveis
 
@@ -37,7 +39,7 @@ void main() {
   } else {
     print("Parar");
   }
-  
+
   if (10 > 5) {
     print("10 é maior que 5");
   }
@@ -68,5 +70,115 @@ void main() {
     print('loop -> $contador');
   }
 
-  //  5 - 
+  //  5 - Métodos e classes
+
+  celular celularDoVictor = celular("Rosa", 3, 0.900, 5.7);
+  celular celularDoLucas = celular("Azul", 5, 0.700, 5.9);
+
+  print(celularDoVictor.toString());
+  print(celularDoLucas.toString());
+
+  double resultado = celularDoVictor.valorDoCelular(1000);
+  print(resultado);
+
+  print(celularDoLucas.valorDoCelular(1000));
+
+  // 6 - Programação orientada a objetos
+
+  Carro Mercedes = Carro('Mercedes');
+  Carro Gol = Carro('Gol');
+
+  Mercedes.modelo;
+  Gol._segredo;
+
+  // 7 - Herança, Polimorfismo e Abstração
+
+  victor Victor = victor();
+  Victor.falar();
+
+  Pagamento pagamento = PagarComBoleto();
+  pagamento.pagar();
+
+  pagamento = PagarComPix();
+  pagamento.pagar();
+
+}
+
+abstract class Pagamento{
+  void pagar();
+}
+
+class PagarComBoleto implements Pagamento{
+
+  void pagar(){
+    print("Pagando com boleto");
+  }
+
+}
+class PagarComPix implements Pagamento{
+  void pagar(){
+    print("Pagando com pix");
+  }
+}
+
+class pai {
+  String falar(){
+    return "Corinthians";
+  }
+}
+
+class victor extends pai{
+
+}
+
+abstract class pessoa {
+  
+  String comunicar();
+
+}
+
+class pessoaET implements pessoa{
+
+  String comunicar(){
+    return "Olá mundo";
+  }
+
+}
+
+class pessoaNaoET implements pessoa{
+
+   String comunicar(){
+    return "Bom dia";
+  }
+
+}
+
+class Carro {
+  final String modelo;
+  String _segredo = "muito dinheiro";
+
+  int _valor = 1000;
+
+  int get valorDoCarro => _valor;
+
+  void set value(int valor) => _valor = valor;
+
+  Carro(this.modelo);
+}
+
+class celular {
+  final String cor;
+  final int qtdPros;
+  final double peso, tamanho;
+
+  celular(this.cor, this.qtdPros, this.peso, this.tamanho);
+
+  @override
+  String toString() {
+    return "cor $cor, qtdPros $qtdPros, Peso $peso, Tamanho $tamanho";
+  }
+
+  double valorDoCelular(double valor) {
+    return valor * qtdPros;
+  }
 }
